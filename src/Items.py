fc02c9cee18af1deb66f9503833b3a5dfb6cb6f2
Alec Liu <sammiliu71@gmail.com>
2025-11-25 11:15:40 -0800

    trader and items classes added, not yet fully implemented

---
+++ src/Items.py
@@ -0,0 +1,12 @@
+import random
+
+class Items:
+    itemType, isRepeating, itemAmount
+
+    def __init__(type, repeat, amount):
+        itemType = type
+        isRepeating = repeat,
+        itemAmount = amount 
+
+    def take():
+        pass
---
+++ src/Trader.py
@@ -0,0 +1,15 @@
+class Trader(Items):
+    traderType
+    def __init__(type):
+        super().__init__("trader", True, float("inf"))
+        traderType = type
+        match traderType:
+            case "agressive":
+                pass
+            case "assertive":
+                pass
+            case "passive":
+                pass
+
+    def bargain():
+        pass
