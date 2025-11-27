c4aa4cc300a5af8af8448ba9c780ddfde3f6251d
JustTransScripts <157331180+JustTransScripts@users.noreply.github.com>
2025-11-27 01:30:10 -0800

    start work on initing the map, add obj referneces for brain

--- src/Brain.py
+++ src/Brain.py
@@ -1,2 +1,9 @@
 class Brain:
-    
+    player_reference = None
+    vision_reference = None
+
+    def attach_player(self, player):
+        self.player_reference = player
+
+    def attach_vision(self, vision):
+        self.vision_reference = vision
--- src/Map.py
+++ src/Map.py
@@ -1,5 +1,42 @@
+import Terrain
+import Tiles
+import Items
+import Trader
+import Player
+
+# TODO: adjust as needed, just example for now
+ITEM_PROBABILITY_TABLE = [0.5, 0.1, 0.01]
+
+
 class Map:
-    def __init__(self, width, height, player):
-        self.map_width = width
-        self.map_height = height
-        self.player = player
+    width = 0
+    height = 0
+    player = None
+    difficulty = -1
+    tile_map = None
+
+    def reset(self, width=0, height=0, player=None, difficulty=-1):
+        regenerate = False
+        if (width > 0):
+            regenerate = True
+            self.width = width
+        if (height > 0):
+            regenerate = True
+            self.height = height
+        if (not(player is None)):
+            self.player = player
+        if (difficulty >= 0):
+            regenerate = True
+            self.difficulty = difficulty
+        if (regenerate):
+            self.tile_map = []
+            for x in range(0, width):
+                current_row = []
+                for y in range(0, height):
+                    # TODO: implement tile generation
+                    current_tile = Tile()
+                    current_row.append(current_tile)
+                self.tile_map.append(current_row)
+
+    def __init__(self, width, height, player, difficulty):
+        self.reset(width, height, player, difficulty)
